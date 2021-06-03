b727844184110dbc976d1aba28322ceebe41c3ec
Mason Boeman <masonboeman@gmail.com>
2021-06-02 21:11:06 -0500

    add adapter functions to the extension traits

--- src/core/event_state_stream/event_state_stream_ext.rs
+++ src/core/event_state_stream/event_state_stream_ext.rs
@@ -1,6 +1,8 @@
+use either::Either;
+
 use crate::core::{transposer::TransposerEngine, Transposer};
 
-use super::EventStateStream;
+use super::{EventStateStream, event_state_map_stream::EventStateMapStream, event_state_split_stream::{self, EventStateSplitLeft, EventStateSplitRight, bounded, unbounded}};
 #[cfg(realtime)]
 use super::{timestamp::Timestamp, RealtimeStream};
 
@@ -23,6 +25,7 @@ pub trait EventStateStreamExt: EventStateStream {
         RealtimeStream::new(self, reference)
     }
 
+    /// Adapter for converting a schedule stream into another via a transposer.
     fn into_engine<
         'tr,
         T: Transposer<
@@ -43,13 +46,45 @@ pub trait EventStateStreamExt: EventStateStream {
         TransposerEngine::new(self, initial)
     }
 
-    // fn split_bounded<L, R>(self, buffer_size: usize) -> (
-    //     EventStateSplitLeft<Self, L, R>,
-    //     EventStateSplitRight<Self, L, R>,
-    // )
-    //     where
-    //         Self::Item= Either<L, R>,
-    // {
-    //     bounded(self, buffer_size)
-    // }
+    /// Adapter for converting the events and states of a stream.
+    fn map<E, S>(
+        self,
+        event_transform: fn(Self::Event) -> E,
+        state_transform: fn(Self::State) -> S,
+    ) -> EventStateMapStream<Self, E, S>
+    where Self: Sized
+    {
+        EventStateMapStream::new(self, event_transform, state_transform)
+    }
+}
+
+impl<S, L, R> EitherStateStreamExt<L, R> for S where S: EventStateStream<Event=Either<L, R>> {}
+
+pub trait EitherStateStreamExt<L, R>: EventStateStream<Event=Either<L, R>>{
+
+    /// Adapter to split a stream of Either<L, R> events into two streams.
+    /// both streams have the full state, but each gets only L or R.
+    ///
+    /// this variant will return pending on one half if the other gets too far ahead.
+    fn split_bounded(self, buffer_size: usize) -> (
+        EventStateSplitLeft<Self, L, R>,
+        EventStateSplitRight<Self, L, R>,
+    )
+        where
+            Self: Sized
+    {
+        bounded(self, buffer_size)
+    }
+
+    /// Adapter to split a stream of Either<L, R> events into two streams.
+    /// both streams have the full state, but each gets only L or R.
+    fn split_unbounded(self) -> (
+        EventStateSplitLeft<Self, L, R>,
+        EventStateSplitRight<Self, L, R>,
+    )
+        where
+            Self: Sized
+    {
+        unbounded(self)
+    }
 }
