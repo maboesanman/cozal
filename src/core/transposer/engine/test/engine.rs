0ccda74e62fa7e098f10c21f1edc7c27b862d2a1
Mason Boeman <masonboeman@gmail.com>
2021-06-06 23:42:30 -0500

    a couple more tests

--- src/core/transposer/engine/test/engine.rs
+++ src/core/transposer/engine/test/engine.rs
@@ -1,12 +1,18 @@
 use std::{pin::Pin, task::Context};
 
-use crate::{core::{event_state_stream::{
+use crate::{
+    core::{
+        event_state_stream::{
             iter_event_state_stream::IterEventStateStream, EventStatePoll, EventStateStream,
             EventStateStreamExt,
-        }, transposer::test::test_transposer::{HandleRecord, TestTransposer}}, test::test_waker::DummyWaker};
+        },
+        transposer::test::test_transposer::{HandleRecord, TestTransposer},
+    },
+    test::test_waker::DummyWaker,
+};
 
 #[test]
-fn basic_test() {
+fn poll() {
     // this event state stream emits an event every 10, with payload i, and state i^2
     let test_input_iter = (0..).map(|i| (10 * i, i, i * i));
     let test_input = IterEventStateStream::new(test_input_iter, 0);
@@ -40,6 +46,40 @@ fn basic_test() {
 }
 
 #[test]
+fn poll_forget() {
+    // this event state stream emits an event every 10, with payload i, and state i^2
+    let test_input_iter = (0..).map(|i| (10 * i, i, i * i));
+    let test_input = IterEventStateStream::new(test_input_iter, 0);
+
+    let mut engine = test_input.into_engine::<_, 20>(TestTransposer::new(vec![]));
+    let engine_ref = &mut engine;
+
+    let mut engine_pin = unsafe { Pin::new_unchecked(engine_ref) };
+
+    let (waker, _) = DummyWaker::new();
+
+    let mut cx = Context::from_waker(&waker);
+
+    let poll = engine_pin.as_mut().poll_forget(15, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Event(0, 0)));
+
+    let poll = engine_pin.as_mut().poll_forget(15, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Event(1, 10)));
+
+    let poll = engine_pin.as_mut().poll_forget(15, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Scheduled(_, 20)));
+
+    let poll = engine_pin.as_mut().poll_forget(35, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Event(4, 20)));
+
+    let poll = engine_pin.as_mut().poll_forget(15, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Scheduled(_, 30)));
+
+    let poll = engine_pin.as_mut().poll_forget(30, &mut cx);
+    assert!(matches!(poll, EventStatePoll::Event(9, 30)));
+}
+
+#[test]
 fn ordering_invariability() {
     // this event state stream emits an event every 10, with payload i, and state i^2
     let items = [1, 3, 0, 2];
@@ -56,12 +96,12 @@ fn ordering_invariability() {
 
     let mut cx = Context::from_waker(&waker);
 
-    engine_pin.as_mut().poll(35, &mut cx);
-    engine_pin.as_mut().poll(35, &mut cx);
-    engine_pin.as_mut().poll(35, &mut cx);
-    engine_pin.as_mut().poll(35, &mut cx);
-    engine_pin.as_mut().poll(35, &mut cx);
-    engine_pin.as_mut().poll(35, &mut cx);
+    loop {
+        match engine_pin.as_mut().poll_events(35, &mut cx) {
+            EventStatePoll::Ready(()) => break,
+            _ => continue,
+        }
+    }
 
     if let EventStatePoll::Ready(state) = engine_pin.as_mut().poll(5, &mut cx) {
         if let HandleRecord::Input(t, v) = &state[0] {
