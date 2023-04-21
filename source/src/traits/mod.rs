a1a95b2e124b92f100e65198233ea213931cbf89
Mason Boeman <masonboeman@gmail.com>
2023-04-21 14:55:18 -0700

    remove concurrent_source

--- source/src/traits/mod.rs
+++ source/src/traits/mod.rs
@@ -1,9 +1,7 @@
-mod concurrent_source;
 mod source;
 mod source_ext;
 mod timestamp;
 
-pub use self::concurrent_source::ConcurrentSource;
 pub use self::source::{Source, SourceContext};
 pub use self::source_ext::SourceExt;
 pub use self::timestamp::Timestamp;
